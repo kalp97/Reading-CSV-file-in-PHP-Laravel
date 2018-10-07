d3fe4aab66f7b8bc3a06d15eecfc75c28e4de52b
kalp97 <kdd32@njit.edu>
2018-10-06 23:23:10 -0400

    Feature: Reading thr csv file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-main::start("exam.csv");
+main::start("example.csv");
 
 
 
@@ -211,7 +211,143 @@ class html
     }
 
 }
+class csv {
 
+
+
+    static public function getRecords($filename) {
+
+
+
+        $file = fopen($filename,"r");
+
+
+
+        $fieldNames = array();
+
+
+
+        $count = 0;
+
+
+
+
+
+        while(! feof($file))
+
+        {
+
+
+
+            $record = fgetcsv($file);
+
+            if($count == 0) {
+
+                $fieldNames = $record;
+
+            } else {
+
+                $records[] = recordFactory::create($fieldNames, $record);
+
+            }
+
+            $count++;
+
+        }
+
+
+
+        fclose($file);
+
+        return $records;
+
+
+
+    }
+
+
+
+}
+
+
+
+class record {
+
+
+
+    public function __construct(Array $fieldNames = null, $values = null )
+
+    {
+
+        $record = array_combine($fieldNames, $values);
+
+
+
+        foreach ($record as $property => $value) {
+
+            $this->createProperty($property, $value);
+
+        }
+
+
+
+    }
+
+
+
+    public function returnArray() {
+
+        $array = (array) $this;
+
+
+
+        return $array;
+
+    }
+
+
+
+    public function createProperty($name = 'first', $value = 'keith') {
+
+
+
+        $this->{$name} = $value;
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
+class recordFactory {
+
+
+
+    public static function create(Array $fieldNames = null, Array $values = null) {
+
+
+
+
+
+        $record = new record($fieldNames, $values);
+
+
+
+        return $record;
+
+
+
+    }
+
+}
 
 
 
