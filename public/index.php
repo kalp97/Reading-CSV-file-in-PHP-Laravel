a9c41ad0cb4c8895821f803e3aea4dd2aad5bfc6
kalp97 <kdd32@njit.edu>
2018-10-06 23:03:54 -0400

    Fix: Adding start function in Main class

--- public/index.php
+++ public/index.php
@@ -1,4 +1,35 @@
 <?php
-echo 'test';
+main::start("exam.csv");
+
+
+
+class main  {
+
+
+
+    static public function start($filename) {
+
+
+
+        $records = csv::getRecords($filename);
+
+
+
+        $cols = table::columns($records);
+
+        $rows = table::rows($records);
+
+
+
+        html::generateTable($cols,$rows);
+
+
+
+
+
+    }
+
+}
+
 ?>
 
