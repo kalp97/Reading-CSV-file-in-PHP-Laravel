e0449e6fca85674dbc895c30291788419d72a051
kalp97 <kdd32@njit.edu>
2018-10-06 23:07:13 -0400

    Feature: Adding static functions rows,columns in table class

--- public/index.php
+++ public/index.php
@@ -31,5 +31,60 @@ class main  {
 
 }
 
+class table {
+
+
+
+    public static function columns($records){
+
+
+
+
+
+        $array = $records[0]->returnArray();
+
+
+
+        return array_keys($array);
+
+    }
+
+
+
+    public  static function rows($records){
+
+
+
+        $rows = [];
+
+        $count = 0;
+
+
+
+        foreach ($records as $record){
+
+
+
+            $array = $record->returnArray();
+
+
+
+            $rows[$count++] = array_values($array);
+
+        }
+
+        return $rows;
+
+
+
+    }
+
+}
+
+
+
+
+
+
 ?>
 
