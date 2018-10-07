0c8362a04e65ea0af72ef042d09989c9906df32d
kalp97 <kdd32@njit.edu>
2018-10-06 22:36:27 -0400

    Feature: Adding start function in Main class

--- public/index.php
+++ public/index.php
@@ -1,3 +1,36 @@
 <?php
 echo 'test';
 ?>
+
+main::start("examplee.csv");
+
+
+
+class main  {
+
+
+
+static public function start($filename) {
+
+
+
+$records = csv::getRecords($filename);
+
+
+
+$cols = table::columns($records);
+
+$rows = table::rows($records);
+
+
+
+html::generateTable($cols,$rows);
+
+
+
+
+
+}
+
+}
+
